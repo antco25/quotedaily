8ad3fc2ca2cd1c3aa1229125d5870905e6ff9692
Anthony <anthonyco@hotmail.com>
2023-01-20 16:53:16 -0500

    2023.01.20 wip

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Quote } from '../../context/dataTypes';
 import quotation from '../Icons/quotation.svg';
+import styles from './Card.module.scss';
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-  data: Quote
+  data: Quote,
+  topCard?: boolean
 }
 
-function Card({ data, className }: CardProps) {
+//TODO: Max length of content
+//TODO: Bookmark symbol
+function Card({ data, topCard, className }: CardProps) {
   return (
     <div className={`${className ? className + ' ' : ''}
-    relative bg-white rounded-lg shadow-lg h-[300px] text-center p-6`}>
+    relative bg-white rounded-lg shadow-lg h-[300px] text-center p-5`}>
       <img src={data.image} className='absolute object-cover rounded-lg w-full h-full left-0 top-0' />
-      <div className='relative h-full flex flex-col justify-center'>
-        <img src={quotation} className='absolute top-0' />
-        <h1 className='font-condensed text-lg'>{data.content}</h1>
-        <p className='text-base italic font-medium absolute w-full bottom-10'>{data.author}</p>
+      <div className='relative h-full flex flex-col justify-center items-center text-white'>
+        <div className={`${styles.cardText} ${topCard ? 'py-8' : 'py-5'} px-6 relative`}>
+          <img src={quotation} className={`absolute top-1 left-1 ${topCard ? 'w-8' : 'w-5'}`} />
+          <h1 className={`${topCard ? 'text-2xl' : 'text-lg'} leading-tight`}>{data.content}</h1>
+          <img src={quotation} className={`absolute bottom-1 right-1 rotate-180 ${topCard ? 'w-8' : 'w-5'}`}/>
+        </div>
+        <p className={`${styles.cardText} ${topCard ? 'text-lg' : 'text-base'} italic mt-3 p-1`}>-{data.author}</p>
       </div>
     </div>
   );
 }
 
 export default Card;
-
-//<img src={quotation} className='rotate-180 absolute' />
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -3,18 +3,26 @@ import Card from '../Card/Card';
 
 import { data } from '../../context/sampleData';
 
+//TODO: Filter
 function Grid() {
   return (
-    <div className='grid grid-cols-3 gap-5'>
-      {
-        data.results.map((q, index) => {
-          if (index === 0)
-            return <Card key={index} data={q} className='col-span-2'/>
-          else
-            return <Card key={index} data={q} />
-        })
-      }
+    <div className='p-5'>
+      <h1 className='font-medium text-5xl mt-10 mb-4'>Quotes of the Day</h1>
+      <div className='mb-8'>
+        <span className='text-white text-center inline-block text-base min-w-[56px] p-2 rounded-lg bg-blue-600'>All</span>
+      </div>
+      <div className='grid grid-cols-3 gap-5'>
+        {
+          data.results.map((q, index) => {
+            if (index === 0)
+              return <Card key={index} data={q} topCard className='col-span-2' />
+            else
+              return <Card key={index} data={q} />
+          })
+        }
+      </div>
     </div>
+
   );
 }
 
